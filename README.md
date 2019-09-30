4099a5af14a54e08337b52674dd30c0a72e74002
suzanagi <realyutanemoto@gmail.com>
2019-09-30 18:39:02 +0900

    initialization

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bing Scraping Module
+A python program implements the scraping function for Bing search engine and returns the result with original type class.
---
+++ bing_search_module.py
@@ -0,0 +1,43 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+from result_item import ResultItem
+
+def search(query):
+    # Prepare a list for returning the search results
+    result = list()
+    # Prepare a parameter for the given query
+    param = {"q": query}
+    try:
+        # Get the Bing search result page for the query
+        rt = requests.get("https://www.bing.com/search", params = param)
+        # Analyse the result page using BeautifulSoup
+        soup = BeautifulSoup(rt.text, "html.parser")
+    except:
+        print("Internet Disconnected. Connect to download text.")
+    # Obtain topics and URL element by the BeautifulSoup function
+    results = soup.find("ol", {"id":"b_results"})
+    lists = results.findAll("li", {"class":"b_algo"})
+    for item in lists:
+        item_text = item.find("a").text
+        item_href = item.find("a").attrs["href"]
+        # Put the results in the list to be returned
+        if item_text and item_href:
+            result.append(ResultItem(item_text, item_href))
+    # Return the result list
+    return result
+
+# Main Function
+if __name__ == "__main__":
+    # Prepare query vairable
+    query = sys.argv[1]
+    # Append multiple query words with "+"
+    for arg in sys.argv[2:]:
+        query = query + "+" + arg
+    # Experiment the search function
+    result = search(query)
+    # Print the result list to the command line
+    for item in result:
+        print("[title] "+item.title)
+        print("[url] "+item.url)
+
